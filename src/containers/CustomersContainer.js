71c3a6ff2bf80aec8ae21b653af09c6ae8f76109
Guido <guido.mourelle@gmail.com>
2018-06-22 23:45:35 -0300

    .

--- src/containers/CustomersContainer.js
+++ src/containers/CustomersContainer.js
@@ -1,37 +1,21 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-//import { withRouter } from "react-router-dom";
-//import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+
 import AppFrame from "./../components/AppFrame";
 import CustomersList from "./../components/CustomersList";
 import CustomersActions from "./../components/CustomersActions";
-// import { fetchCustomers } from "./../actions/fetchCustomers";
-// import { getCustomers } from "./../selectors/customers";
-
-const customers = [
-  {
-    dni: "27000000",
-    name: "Juan",
-    age: 37
-  },
-  {
-    dni: "28000000",
-    name: "Sergio",
-    age: 37
-  },
-  {
-    dni: "29000000",
-    name: "Maria",
-    age: 37
-  }
-];
+import { fetchCustomers } from "./../actions/fetchCustomers";
+import { getCustomers } from "./../selectors/customers";
 
 class CustomersContainer extends Component {
-  //   componentDidMount() {
-  //     if (this.props.customers.length === 0) {
-  //       this.props.fetchCustomers();
-  //     }
-  //   }
+  componentDidMount() {
+    // if (this.props.customers.length === 0) {
+    //   this.props.fetchCustomers();
+    // }
+    this.props.fetchCustomers();
+  }
 
   handleAddNew = () => {
     this.props.history.push("/customers/new");
@@ -52,7 +36,7 @@ class CustomersContainer extends Component {
         <AppFrame
           header={"Listado de clientes"}
           //body={this.renderBody(this.props.customers)}>
-          body={this.renderBody(customers)}
+          body={this.renderBody(this.props.customers)}
         />
       </div>
     );
@@ -64,19 +48,19 @@ CustomersContainer.propTypes = {
   customers: PropTypes.array.isRequired
 };
 
-// CustomersContainer.defaultProps = {
-//   customers: []
-// };
+CustomersContainer.defaultProps = {
+  customers: []
+};
 
-// const mapStateToProps = state => ({
-//   customers: getCustomers(state)
-// });
+const mapStateToProps = state => ({
+  customers: getCustomers(state)
+});
 
-// export default withRouter(
-//   connect(
-//     mapStateToProps,
-//     { fetchCustomers }
-//   )(CustomersContainer)
-// );
+export default withRouter(
+  connect(
+    mapStateToProps,
+    { fetchCustomers }
+  )(CustomersContainer)
+);
 
-export default CustomersContainer;
+//export default withRouter(CustomersContainer);
